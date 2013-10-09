fa699d8d0a3a0adbc01ad02104d6a1c4884006a7
Theeban Soundararajan <theeban@intersect.org.au>
2013-10-09 16:02:32 +1100

    SKYM-7: refactoring query generator

--- app/models/queries/query_generator.rb
+++ app/models/queries/query_generator.rb
@@ -1,9 +1,9 @@
 class QueryGenerator
 
-  def self.generate_point_query(dataset, args)
+  def self.generate_point_query(args)
     registry = Rails.application.config.asvo_registry
-    registry_dataset = registry.datasets[dataset]
-    catalogue = registry_dataset[:catalogues][args[:catalogue]]
+    dataset = registry.datasets[args[:dataset]]
+    catalogue = dataset[:catalogues][args[:catalogue]]
     args = {
       table_name: catalogue[:table_name],
       ra_column_name: catalogue[:ra_column_name],
--- spec/models/queries/query_generator_spec.rb
+++ spec/models/queries/query_generator_spec.rb
@@ -4,12 +4,13 @@ describe QueryGenerator do
 
   it 'Generates point query for dataset skymapper and catalogue fs' do
     args = {
+        dataset: :skymapper,
         catalogue: :fs,
         ra: 90.0,
         dec: -1.0,
         sr: 1.0
     }
-    point_query = QueryGenerator.generate_point_query(:skymapper, args)
+    point_query = QueryGenerator.generate_point_query(args)
     point_query.valid?.should be_true
   end
 
