2b149099ff5d2655a3f093b9cbec87a07a7ebd56
Theeban Soundararajan <theeban@intersect.org.au>
2013-10-16 09:18:39 +1100

    SKYM-7: updated cap deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,8 +6,8 @@ require 'rvm/capistrano'
 set :keep_releases, 5
 set :application, 'asvo-skymapper'
 set :stages, %w(qa staging production)
-set :default_stage, "qa"
-set :rpms, "openssl openssl-devel curl-devel httpd-devel apr-devel apr-util-devel zlib zlib-devel libxml2 libxml2-devel libxslt libxslt-devel libffi mod_ssl mod_xsendfile"
+set :default_stage, 'qa'
+set :rpms, 'openssl openssl-devel curl-devel httpd-devel apr-devel apr-util-devel zlib zlib-devel libxml2 libxml2-devel libxslt libxslt-devel libffi mod_ssl mod_xsendfile'
 set :shared_children, shared_children + %w(log_archive)
 set :shell, '/bin/bash'
 set :rvm_ruby_string, 'ruby-2.0.0-p247@skymapper'
@@ -17,12 +17,13 @@ set :rvm_type, :user
 set :scm, 'git'
 set :repository, 'git@github.com:IntersectAustralia/asvo-skymapper.git'
 set :deploy_via, :copy
-set :copy_exclude, [".git/*"]
+set :copy_exclude, ['.git/*']
+set :passenger_version, '4.0.20'
 
 set :branch do
   default_tag = 'master'
 
-  puts "Available tags:"
+  puts 'Available tags:'
   puts `git tag`
   tag = Capistrano::CLI.ui.ask "Tag to deploy (make sure to push the branch/tag first) or HEAD?: [#{default_tag}] "
   tag = default_tag if tag.empty?
@@ -33,6 +34,7 @@ set(:user) { "#{defined?(user) ? user : 'devel'}" }
 set(:group) { "#{defined?(group) ? group : user}" }
 set(:user_home) { "/home/#{user}" }
 set(:deploy_to) { "#{user_home}/#{application}" }
+
 default_run_options[:pty] = true
 
 namespace :server_setup do
@@ -57,11 +59,11 @@ namespace :server_setup do
     end
   end
   task :gem_install, :roles => :app do
-    run "gem install bundler"
-    run "gem install passenger --version 4.0.20"
+    run 'gem install bundler'
+    run "gem install passenger --version #{passenger_version}"
   end
   task :passenger, :roles => :app do
-    run "passenger-install-apache2-module -a"
+    run 'passenger-install-apache2-module -a'
   end
   namespace :config do
     task :apache do
@@ -125,7 +127,7 @@ namespace :deploy do
     restart
   end
 
-  desc "Additional Symlinks to shared_path"
+  desc 'Additional Symlinks to shared_path'
   task :additional_symlinks do
     run "rm -rf #{release_path}/tmp/shared_config"
     run "mkdir -p #{shared_path}/env_config"
@@ -136,25 +138,25 @@ namespace :deploy do
   end
 
   # Load the schema
-  desc "Load the schema into the database (WARNING: destructive!)"
+  desc 'Load the schema into the database (WARNING: destructive!)'
   task :schema_load, :roles => :db do
-    run("cd #{current_path} && bundle exec rake db:schema:load", :env => {'RAILS_ENV' => "#{stage}"})
+    run("cd #{current_path} && exec rake db:schema:load", :env => {'RAILS_ENV' => "#{stage}"})
   end
 
   # Run the sample data populator
-  desc "Run the test data populator script to load test data into the db (WARNING: destructive!)"
+  desc 'Run the test data populator script to load test data into the db (WARNING: destructive!)'
   task :populate, :roles => :db do
     generate_populate_yml
-    run("cd #{current_path} && bundle exec rake db:populate", :env => {'RAILS_ENV' => "#{stage}"})
+    run("cd #{current_path} && exec rake db:populate", :env => {'RAILS_ENV' => "#{stage}"})
   end
 
   # Seed the db
-  desc "Run the seeds script to load seed data into the db (WARNING: destructive!)"
+  desc 'Run the seeds script to load seed data into the db (WARNING: destructive!)'
   task :seed, :roles => :db do
-    run("cd #{current_path} && bundle exec rake db:seed", :env => {'RAILS_ENV' => "#{stage}"})
+    run("cd #{current_path} && exec rake db:seed", :env => {'RAILS_ENV' => "#{stage}"})
   end
 
-  desc "Full redepoyment, it runs deploy:update and deploy:refresh_db"
+  desc 'Full redepoyment, it runs deploy:update and deploy:refresh_db'
   task :full_redeploy do
     update
     rebundle
@@ -167,7 +169,7 @@ namespace :deploy do
 
     # Prompt to refresh_db on unless we're in QA
     if stage.eql?(:qa)
-      input = "yes"
+      input = 'yes'
     else
       puts "This step (deploy:refresh_db) will erase all data and start from scratch.\nYou probably don't want to do it. Are you sure?' [NO/yes]".colorize(:red)
       input = STDIN.gets.chomp
@@ -178,11 +180,11 @@ namespace :deploy do
       seed
       populate
     else
-      puts "Skipping database nuke"
+      puts 'Skipping database nuke'
     end
   end
 
-  desc "Safe redeployment"
+  desc 'Safe redeployment'
   task :safe do # TODO roles?
     require 'colorize'
     update
@@ -192,7 +194,7 @@ namespace :deploy do
     puts cat_migrations_output
 
     if cat_migrations_output != '0 pending migration(s)'
-      print "There are pending migrations. Are you sure you want to continue? [NO/yes] ".colorize(:red)
+      print 'There are pending migrations. Are you sure you want to continue? [NO/yes] '.colorize(:red)
       abort "Exiting because you didn't type 'yes'" unless STDIN.gets.chomp == 'yes'
     end
 
@@ -206,50 +208,38 @@ end
 
 namespace :backup do
   namespace :db do
-    desc "make a database backup"
+    desc 'make a database backup'
     task :dump do
       run "cd #{current_path} && rake db:backup", :env => {'RAILS_ENV' => stage}
     end
 
-    desc "trim database backups"
+    desc 'trim database backups'
     task :trim do
       run "cd #{current_path} && rake db:trim_backups", :env => {'RAILS_ENV' => stage}
     end
   end
 end
 
-desc "Give sample users a custom password"
+desc 'Give sample users a custom password'
 task :generate_populate_yml, :roles => :app do
-  require "yaml"
+  require 'yaml'
   require 'colorize'
 
-  puts "Set sample user password? (required on initial deploy) [NO/yes]".colorize(:red)
+  puts 'Set sample user password? (required on initial deploy) [NO/yes]'.colorize(:red)
   input = STDIN.gets.chomp
   do_set_password if input.match(/^yes/)
 end
 
-namespace :worker do
-  desc "Start workers"
-  task :start do
-    run "cd #{current_path} && script/delayed_job start", :env => {'RAILS_ENV' => stage}
-  end
-
-  desc "Stop workers"
-  task :stop do
-    run "cd #{current_path} && script/delayed_job stop", :env => {'RAILS_ENV' => stage}
-  end
-end
-
-desc "Helper method that actually sets the sample user password"
+desc 'Helper method that actually sets the sample user password'
 task :do_set_password, :roles => :app do
-  set :custom_sample_password, proc { Capistrano::CLI.password_prompt("Sample User password: ") }
+  set :custom_sample_password, proc { Capistrano::CLI.password_prompt('Sample User password: ') }
   buffer = Hash[:password => custom_sample_password]
   put YAML::dump(buffer), "#{shared_path}/env_config/sample_password.yml", :mode => 0664
 end
 
-desc "After updating code we need to populate a new database.yml"
+desc 'After updating code we need to populate a new database.yml'
 task :generate_database_yml, :roles => :app do
-  require "yaml"
+  require 'yaml'
 
   buffer = YAML::load_file('config/database.yml')
 
@@ -264,7 +254,7 @@ task :generate_database_yml, :roles => :app do
 
   # Populate production password
   if rails_env == 'production'
-    set :production_database_password, proc { Capistrano::CLI.password_prompt("Database password: ") }
+    set :production_database_password, proc { Capistrano::CLI.password_prompt('Database password: ') }
     buffer[rails_env]['password'] = production_database_password
   end
 
