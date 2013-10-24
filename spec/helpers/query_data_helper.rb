0b9e1793215b36f722ce00cf7b4c464ecd17fc8e
Theeban Soundararajan <theeban@intersect.org.au>
2013-10-24 16:04:01 +1100

    SKYM-26: refactored query data helper

--- spec/helpers/query_data_helper.rb
+++ spec/helpers/query_data_helper.rb
@@ -1,333 +1,90 @@
-def save_query_fixture(filename, query_args, service_args, factory)
-  query = factory.call(query_args)
+def save_query_fixture(data)
+  query_args = {
+      dataset: data[:dataset],
+      catalogue: data[:catalogue]
+  }
+  query_args.merge!(data[:params])
+
+  service_args = {
+      dataset: data[:dataset],
+      catalogue: data[:catalogue]
+  }
+
+  query = QueryGenerator.method(data[:method]).call(query_args)
   service = SyncQueryService.new(service_args)
   res = service.fetch_query_response(query)
-  xml = File.new(Rails.root.join("spec/fixtures/#{filename}.xml"), 'w')
+  xml = File.new(Rails.root.join("spec/fixtures/#{data[:filename]}.xml"), 'w')
   xml.write(res.body)
   xml.close
-  vo = File.new(Rails.root.join("spec/fixtures/#{filename}.vo"), 'w')
-  vo_table = VOTableParser.parse_xml(File.read(Rails.root.join("spec/fixtures/#{filename}.xml")))
-  puts "#{filename} contains #{vo_table.table_data ? vo_table.table_data.size : 0} objects"
+  vo = File.new(Rails.root.join("spec/fixtures/#{data[:filename]}.vo"), 'w')
+  vo_table = VOTableParser.parse_xml(File.read(Rails.root.join("spec/fixtures/#{data[:filename]}.xml")))
+  puts "#{data[:filename]} contains #{vo_table.table_data ? vo_table.table_data.size : 0} objects"
   YAML.dump(vo_table, vo)
   vo.close
 end
 
-def generate_point_query_fixtures
+def generate_query_fixtures
 
-  query_args = {
-    dataset: 'skymapper',
-    catalogue: 'fs',
-    ra: '178.83871',
-    dec: '-1.18844',
-    sr: '0.5'
-  }
+  query_data = [
+      {dataset: 'skymapper', catalogue: 'fs', params: { ra: '178.83871', dec: '-1.18844', sr: '0.5' }, filename: 'skymapper_point_query_fs_1', method: :generate_point_query },
+      {dataset: 'skymapper', catalogue: 'fs', params: { ra: '1', dec: '1', sr: '1' }, filename: 'skymapper_point_query_fs_2', method: :generate_point_query },
+      {dataset: 'skymapper', catalogue: 'fs', params: { ra: '178.83871', dec: '-1.18844', sr: '2' }, filename: 'skymapper_point_query_fs_3', method: :generate_point_query },
+      {dataset: 'skymapper', catalogue: 'ms', params: { ra: '178.83871', dec: '-1.18844', sr: '0.15' }, filename: 'skymapper_point_query_ms_1', method: :generate_point_query },
+      {dataset: 'skymapper', catalogue: 'ms', params: { ra: '1', dec: '1', sr: '1' }, filename: 'skymapper_point_query_ms_2', method: :generate_point_query },
+      {dataset: 'skymapper', catalogue: 'ms', params: { ra: '178.83871', dec: '-1.18844', sr: '0.5' }, filename: 'skymapper_point_query_ms_3', method: :generate_point_query },
 
-  service_args = {
-    dataset: 'skymapper',
-    catalogue: 'fs',
-  }
-
-  save_query_fixture('skymapper_point_query_fs_1', query_args, service_args, QueryGenerator.method(:generate_point_query))
-
-  query_args = {
-    dataset: 'skymapper',
-    catalogue: 'fs',
-    ra: '1',
-    dec: '1',
-    sr: '1'
-  }
-
-  service_args = {
-    dataset: 'skymapper',
-    catalogue: 'fs',
-  }
-
-  save_query_fixture('skymapper_point_query_fs_2', query_args, service_args, QueryGenerator.method(:generate_point_query))
-
-  query_args = {
-    dataset: 'skymapper',
-    catalogue: 'fs',
-    ra: '178.83871',
-    dec: '-1.18844',
-    sr: '2'
-  }
-
-  service_args = {
-    dataset: 'skymapper',
-    catalogue: 'fs',
-  }
-
-  save_query_fixture('skymapper_point_query_fs_3', query_args, service_args, QueryGenerator.method(:generate_point_query))
-
-  query_args = {
-    dataset: 'skymapper',
-    catalogue: 'ms',
-    ra: '178.83871',
-    dec: '-1.18844',
-    sr: '0.15'
-  }
-
-  service_args = {
-    dataset: 'skymapper',
-    catalogue: 'ms',
-  }
-
-  save_query_fixture('skymapper_point_query_ms_1', query_args, service_args, QueryGenerator.method(:generate_point_query))
-
-  query_args = {
-    dataset: 'skymapper',
-    catalogue: 'ms',
-    ra: '1',
-    dec: '1',
-    sr: '1'
-  }
-
-  service_args = {
-    dataset: 'skymapper',
-    catalogue: 'ms',
-  }
-
-  save_query_fixture('skymapper_point_query_ms_2', query_args, service_args, QueryGenerator.method(:generate_point_query))
-
-  query_args = {
-    dataset: 'skymapper',
-    catalogue: 'ms',
-    ra: '178.83871',
-    dec: '-1.18844',
-    sr: '0.5'
-  }
-
-  service_args = {
-    dataset: 'skymapper',
-    catalogue: 'ms',
-  }
-
-  save_query_fixture('skymapper_point_query_ms_3', query_args, service_args, QueryGenerator.method(:generate_point_query))
-
-end
-
-def generate_point_query_filter_fixtures
+      {dataset: 'skymapper', catalogue: 'fs', params: { ra_min: '1.75', ra_max: '2.25', dec_min: '-2.25', dec_max: '-0.75' }, filename: 'skymapper_rectangular_query_fs_1', method: :generate_rectangular_query },
+      {dataset: 'skymapper', catalogue: 'fs', params: { ra_min: '1', ra_max: '1.1', dec_min: '1', dec_max: '1.1' }, filename: 'skymapper_rectangular_query_fs_2', method: :generate_rectangular_query },
+      {dataset: 'skymapper', catalogue: 'fs', params: { ra_min: '0', ra_max: '10', dec_min: '-2.25', dec_max: '-0.75' }, filename: 'skymapper_rectangular_query_fs_3', method: :generate_rectangular_query },
+      {dataset: 'skymapper', catalogue: 'ms', params: { ra_min: '1.975', ra_max: '2.025', dec_min: '-1.525', dec_max: '-1.475' }, filename: 'skymapper_rectangular_query_ms_1', method: :generate_rectangular_query },
+      {dataset: 'skymapper', catalogue: 'ms', params: { ra_min: '1', ra_max: '1.1', dec_min: '1', dec_max: '1.1' }, filename: 'skymapper_rectangular_query_ms_2', method: :generate_rectangular_query },
+      {dataset: 'skymapper', catalogue: 'ms', params: { ra_min: '1.75', ra_max: '2.25', dec_min: '-2.25', dec_max: '-0.75' }, filename: 'skymapper_rectangular_query_ms_3', method: :generate_rectangular_query }
+  ]
 
   ['u', 'v', 'g', 'r', 'i', 'z'].each do |filter|
 
-    query_args = {
-        dataset: 'skymapper',
-        catalogue: 'fs',
-        ra: '178.83871',
-        dec: '-1.18844',
-        sr: '0.5'
-    }
-    query_args["#{filter}_min".to_sym] = 50
+    filter_data = [
+        { dataset: 'skymapper', catalogue: 'fs', params: { ra: '178.83871', dec: '-1.18844', sr: '0.5', "#{filter}_min" => '50' }.symbolize_keys, filename: "skymapper_point_query_fs_#{filter}_filter_1", method: :generate_point_query },
+        { dataset: 'skymapper', catalogue: 'fs', params: { ra: '178.83871', dec: '-1.18844', sr: '0.5', "#{filter}_max" => '1000' }.symbolize_keys, filename: "skymapper_point_query_fs_#{filter}_filter_2", method: :generate_point_query },
+        { dataset: 'skymapper', catalogue: 'fs', params: { ra: '178.83871', dec: '-1.18844', sr: '0.5', "#{filter}_min" => '50', "#{filter}_max" => '1000' }.symbolize_keys, filename: "skymapper_point_query_fs_#{filter}_filter_3", method: :generate_point_query },
+        { dataset: 'skymapper', catalogue: 'ms', params: { ra: '178.83871', dec: '-1.18844', sr: '0.15', "#{filter}_min" => '0.1' }.symbolize_keys, filename: "skymapper_point_query_ms_#{filter}_filter_1", method: :generate_point_query },
+        { dataset: 'skymapper', catalogue: 'ms', params: { ra: '178.83871', dec: '-1.18844', sr: '0.15', "#{filter}_max" => '1' }.symbolize_keys, filename: "skymapper_point_query_ms_#{filter}_filter_2", method: :generate_point_query },
+        { dataset: 'skymapper', catalogue: 'ms', params: { ra: '178.83871', dec: '-1.18844', sr: '0.15', "#{filter}_min" => '0.1', "#{filter}_max" => '1' }.symbolize_keys, filename: "skymapper_point_query_ms_#{filter}_filter_3", method: :generate_point_query },
 
-    service_args = {
-        dataset: 'skymapper',
-        catalogue: 'fs',
-    }
+        {dataset: 'skymapper', catalogue: 'fs', params: { ra_min: '1.75', ra_max: '2.25', dec_min: '-2.25', dec_max: '-0.75', "#{filter}_min" => '50' }, filename: "skymapper_rectangular_query_fs_#{filter}_filter_1", method: :generate_rectangular_query },
+        {dataset: 'skymapper', catalogue: 'fs', params: { ra_min: '1.75', ra_max: '2.25', dec_min: '-2.25', dec_max: '-0.75', "#{filter}_max" => '1000' }, filename: "skymapper_rectangular_query_fs_#{filter}_filter_2", method: :generate_rectangular_query },
+        {dataset: 'skymapper', catalogue: 'fs', params: { ra_min: '1.75', ra_max: '2.25', dec_min: '-2.25', dec_max: '-0.75', "#{filter}_min" => '50', "#{filter}_max" => '1000' }, filename: "skymapper_rectangular_query_fs_#{filter}_filter_3", method: :generate_rectangular_query },
+        {dataset: 'skymapper', catalogue: 'ms', params: { ra_min: '1.975', ra_max: '2.025', dec_min: '-1.525', dec_max: '-1.475', "#{filter}_min" => '0.1' }, filename: "skymapper_rectangular_query_ms_#{filter}_filter_1", method: :generate_rectangular_query },
+        {dataset: 'skymapper', catalogue: 'ms', params: { ra_min: '1.975', ra_max: '2.025', dec_min: '-1.525', dec_max: '-1.475', "#{filter}_max" => '1' }, filename: "skymapper_rectangular_query_ms_#{filter}_filter_2", method: :generate_rectangular_query },
+        {dataset: 'skymapper', catalogue: 'ms', params: { ra_min: '1.975', ra_max: '2.025', dec_min: '-1.525', dec_max: '-1.475', "#{filter}_min" => '0.1', "#{filter}_max" => '1' }, filename: "skymapper_rectangular_query_ms_#{filter}_filter_3", method: :generate_rectangular_query }
+    ]
 
-    save_query_fixture("skymapper_point_query_fs_#{filter}_filter_1", query_args, service_args, QueryGenerator.method(:generate_point_query))
-
-    query_args = {
-        dataset: 'skymapper',
-        catalogue: 'fs',
-        ra: '178.83871',
-        dec: '-1.18844',
-        sr: '0.5'
-    }
-    query_args["#{filter}_max".to_sym] = 1000
-
-    save_query_fixture("skymapper_point_query_fs_#{filter}_filter_2", query_args, service_args, QueryGenerator.method(:generate_point_query))
-
-    query_args = {
-        dataset: 'skymapper',
-        catalogue: 'fs',
-        ra: '178.83871',
-        dec: '-1.18844',
-        sr: '0.5'
-    }
-    query_args["#{filter}_min".to_sym] = 50
-    query_args["#{filter}_max".to_sym] = 1000
-
-    save_query_fixture("skymapper_point_query_fs_#{filter}_filter_3", query_args, service_args, QueryGenerator.method(:generate_point_query))
-
-    query_args = {
-        dataset: 'skymapper',
-        catalogue: 'ms',
-        ra: '178.83871',
-        dec: '-1.18844',
-        sr: '0.15'
-    }
-    query_args["#{filter}_min".to_sym] = 0.1
-
-    service_args = {
-        dataset: 'skymapper',
-        catalogue: 'ms',
-    }
-
-    save_query_fixture("skymapper_point_query_ms_#{filter}_filter_1", query_args, service_args, QueryGenerator.method(:generate_point_query))
-
-    query_args = {
-        dataset: 'skymapper',
-        catalogue: 'ms',
-        ra: '178.83871',
-        dec: '-1.18844',
-        sr: '0.15'
-    }
-    query_args["#{filter}_max".to_sym] = 1
-
-    save_query_fixture("skymapper_point_query_ms_#{filter}_filter_2", query_args, service_args, QueryGenerator.method(:generate_point_query))
-
-    query_args = {
-        dataset: 'skymapper',
-        catalogue: 'ms',
-        ra: '178.83871',
-        dec: '-1.18844',
-        sr: '0.15'
-    }
-    query_args["#{filter}_min".to_sym] = 0.1
-    query_args["#{filter}_max".to_sym] = 1
-
-    save_query_fixture("skymapper_point_query_ms_#{filter}_filter_3", query_args, service_args, QueryGenerator.method(:generate_point_query))
+    query_data = query_data.concat(filter_data)
 
   end
 
-  query_args = {
-      dataset: 'skymapper',
-      catalogue: 'fs',
-      ra: '178.83871',
-      dec: '-1.18844',
-      sr: '0.5'
-  }
+  filter_all_data = [
+      { dataset: 'skymapper', catalogue: 'fs', params: { ra: '178.83871', dec: '-1.18844', sr: '0.5' }, filename: 'skymapper_point_query_fs_filter_all', method: :generate_point_query },
+      { dataset: 'skymapper', catalogue: 'ms', params: { ra: '178.83871', dec: '-1.18844', sr: '0.15' }, filename: 'skymapper_point_query_ms_filter_all', method: :generate_point_query },
+      { dataset: 'skymapper', catalogue: 'fs', params: { ra_min: '1.75', ra_max: '2.25', dec_min: '-2.25', dec_max: '-0.75' }, filename: 'skymapper_rectangular_query_fs_filter_all', method: :generate_rectangular_query },
+      { dataset: 'skymapper', catalogue: 'ms', params: { ra_min: '1.75', ra_max: '2.25', dec_min: '-2.25', dec_max: '-0.75' }, filename: 'skymapper_rectangular_query_ms_filter_all', method: :generate_rectangular_query }
+  ]
 
   ['u', 'v', 'g', 'r', 'i', 'z'].each do |filter|
-    query_args["#{filter}_min".to_sym] = 50
-    query_args["#{filter}_max".to_sym] = 1000
+    filter_all_data[0][:params]["#{filter}_min".to_sym] = 50
+    filter_all_data[0][:params]["#{filter}_max".to_sym] = 1000
+    filter_all_data[1][:params]["#{filter}_min".to_sym] = 0.1
+    filter_all_data[1][:params]["#{filter}_max".to_sym] = 1
+    filter_all_data[2][:params]["#{filter}_min".to_sym] = 50
+    filter_all_data[2][:params]["#{filter}_max".to_sym] = 1000
+    filter_all_data[3][:params]["#{filter}_min".to_sym] = 0.1
+    filter_all_data[3][:params]["#{filter}_max".to_sym] = 1
   end
 
-  service_args = {
-      dataset: 'skymapper',
-      catalogue: 'fs',
-  }
+  query_data = query_data.concat(filter_all_data)
 
-  save_query_fixture("skymapper_point_query_fs_filter_all", query_args, service_args, QueryGenerator.method(:generate_point_query))
-
-  query_args = {
-      dataset: 'skymapper',
-      catalogue: 'ms',
-      ra: '178.83871',
-      dec: '-1.18844',
-      sr: '0.15'
-  }
-
-  ['u', 'v', 'g', 'r', 'i', 'z'].each do |filter|
-    query_args["#{filter}_min".to_sym] = 0.1
-    query_args["#{filter}_max".to_sym] = 1
+  query_data.each do |data|
+    save_query_fixture(data)
   end
-
-  service_args = {
-      dataset: 'skymapper',
-      catalogue: 'ms',
-  }
-
-  save_query_fixture("skymapper_point_query_ms_filter_all", query_args, service_args, QueryGenerator.method(:generate_point_query))
-
-end
-
-def generate_rectangular_query_fixtures
-
-  query_args = {
-      dataset: 'skymapper',
-      catalogue: 'fs',
-      ra_min: '1.75',
-      ra_max: '2.25',
-      dec_min: '-2.25',
-      dec_max: '-0.75'
-  }
-
-  service_args = {
-      dataset: 'skymapper',
-      catalogue: 'fs',
-  }
-
-  save_query_fixture('skymapper_rectangular_query_fs_1', query_args, service_args, QueryGenerator.method(:generate_rectangular_query))
-
-  query_args = {
-      dataset: 'skymapper',
-      catalogue: 'fs',
-      ra_min: '1',
-      ra_max: '1',
-      dec_min: '1',
-      dec_max: '1'
-  }
-
-  service_args = {
-      dataset: 'skymapper',
-      catalogue: 'fs',
-  }
-
-  save_query_fixture('skymapper_rectangular_query_fs_2', query_args, service_args, QueryGenerator.method(:generate_rectangular_query))
-
-  query_args = {
-      dataset: 'skymapper',
-      catalogue: 'fs',
-      ra_min: '0',
-      ra_max: '10',
-      dec_min: '-2.25',
-      dec_max: '-0.75'
-  }
-
-  service_args = {
-      dataset: 'skymapper',
-      catalogue: 'fs',
-  }
-
-  save_query_fixture('skymapper_rectangular_query_fs_3', query_args, service_args, QueryGenerator.method(:generate_rectangular_query))
-
-  query_args = {
-      dataset: 'skymapper',
-      catalogue: 'ms',
-      ra_min: '1.975',
-      ra_max: '2.025',
-      dec_min: '-1.525',
-      dec_max: '-1.475'
-  }
-
-  service_args = {
-      dataset: 'skymapper',
-      catalogue: 'ms',
-  }
-
-  save_query_fixture('skymapper_rectangular_query_ms_1', query_args, service_args, QueryGenerator.method(:generate_rectangular_query))
-
-  query_args = {
-      dataset: 'skymapper',
-      catalogue: 'ms',
-      ra_min: '1',
-      ra_max: '1',
-      dec_min: '1',
-      dec_max: '1'
-  }
-
-  service_args = {
-      dataset: 'skymapper',
-      catalogue: 'ms',
-  }
-
-  save_query_fixture('skymapper_rectangular_query_ms_2', query_args, service_args, QueryGenerator.method(:generate_rectangular_query))
-
-  query_args = {
-      dataset: 'skymapper',
-      catalogue: 'ms',
-      ra_min: '1.75',
-      ra_max: '2.25',
-      dec_min: '-2.25',
-      dec_max: '-0.75'
-  }
-
-  service_args = {
-      dataset: 'skymapper',
-      catalogue: 'ms',
-  }
-
-  save_query_fixture('skymapper_rectangular_query_ms_3', query_args, service_args, QueryGenerator.method(:generate_rectangular_query))
 
 end
