4310c576239f034b39c1af476a8c897cfff64c25
Theeban Soundararajan <theeban@intersect.org.au>
2013-10-31 11:19:14 +1100

    updated secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-AsvoSkymapper::Application.config.secret_token = '2d24278ac298a92283a105d3b6cd55372b35c558561db4627677554f0a0958d18c5b3bb6651f8c148d37d99bc775abb0f126bfcf57b2dcc1c34fe5f70686c1cd'
+AsvoSkymapper::Application.config.secret_token = 'REPLACE_THIS_WITH_REAL_GOOD_RANDOM_STRING'
