7952b43423d509b96e4663bd504803c8313ea3e6
PrzemyslawP <przemyslaw@intersect.org.au>
2014-03-24 15:29:28 +1100

    Refactor queries

--- app/models/queries/image_query.rb
+++ app/models/queries/image_query.rb
@@ -3,9 +3,11 @@ class ImageQuery < Query
   PARAMETER_FIELDS = [:ra, :dec]
 
   attr_accessor *PARAMETER_FIELDS
+  validates :ra, presence: true
+  validate :ra, :ra_format_and_value
+  validates :dec, presence: true
+  validate :dec, :dec_format_and_value
 
-  validates :ra, presence: true, numericality: { greater_than_or_equal_to: 0, less_than: 360 }, format: { with: /^-?\d*(\.\d{1,6})?$/ }
-  validates :dec, presence: true, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }, format: { with: /^-?\d*(\.\d{1,6})?$/ }
 
   before_validation :clean_values
 
