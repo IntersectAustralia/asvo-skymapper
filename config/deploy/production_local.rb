ce1de95944da445422805e4706b719116c111105
Ilya Anisimoff <ilya@intersect.org.au>
2014-04-16 10:16:34 +1000

    MISC: Defines roles so deployments can be tracked in deployment tracker

--- config/deploy/production_local.rb
+++ config/deploy/production_local.rb
@@ -6,8 +6,8 @@ set :remote_copy_dir, "/tmp"
 set :rails_env, "production"
 set :stage, "production"
 # Your HTTP server, Apache/etc
-role :web, ''
+role :web, 'skymapper.asvo.org.au'
 # This may be the same as your Web server
-role :app, ''
+role :app, 'skymapper.asvo.org.au'
 # This is where Rails migrations will run
-role :db,  '', :primary => true
+role :db,  'skymapper.asvo.org.au', :primary => true
